332227e4c4600c88f48c20222f58d481a17b0521
Gerald Bual <166917962+Shiwad@users.noreply.github.com>
2026-06-06 21:36:46 +0800

    chore: add deployment configs and fix env var bug

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 4000;
 await connectDB()
 await connectCloudinary()
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', process.env.FRONTEND_URL].filter(Boolean)
 
 app.use(express.json());
 app.use(cookieParser());
